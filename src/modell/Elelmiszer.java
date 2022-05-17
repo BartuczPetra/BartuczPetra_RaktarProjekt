2c7060601fce4dd5f5824b355b1ad9976ea211d9
bartuczpetra <bartucz.petra@gmail.com>
2022-05-17 12:25:49 +0200

    modell package Classok adattagjai, metodusai

--- src/modell/Elelmiszer.java
+++ src/modell/Elelmiszer.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modell;
 
-/**
- *
- * @author vizsgaszf
- */
-public class Elelmiszer {
+import java.text.Collator;
+import java.time.LocalDate;
+import java.util.Comparator;
+
+class NevComparator implements Comparator<Elelmiszer>{
+    @Override
+    public int compare(Elelmiszer o1, Elelmiszer o2) {
+        Collator coll = Collator.getInstance();
+        return coll.compare(o1.getNev(), o2.getNev());
+    } 
+}
+
+class GyartoComparator implements Comparator<Elelmiszer>{
+    @Override
+    public int compare(Elelmiszer o1, Elelmiszer o2) {
+        Collator coll = Collator.getInstance();
+        return coll.compare(o1.getGyarto(), o2.getGyarto());
+    } 
+}
+
+public class Elelmiszer{
+    private String nev, gyarto;
+    private LocalDate lejaratiDatum;
+    
+    public Elelmiszer(String nev, String gyarto) throws HibasDatumException {
+        this(nev,gyarto, LocalDate.now().plusYears(1));
+    }
+
+    public Elelmiszer(String nev, String gyarto, LocalDate lejaratiDatum) throws HibasDatumException {
+        if(this.lejaratiDatum.isBefore(LocalDate.now())){
+            throw new HibasDatumException("Lejárt élelmiszer!");
+        }
+        this.nev = nev;
+        this.gyarto = gyarto;
+        this.lejaratiDatum = lejaratiDatum;
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public String getGyarto() {
+        return gyarto;
+    }
+
+    public LocalDate getLejaratiDatum() {
+        return lejaratiDatum;
+    }
+
+    public static NevComparator NevRendezo(){
+        return new NevComparator();
+    }
+    
+    public static GyartoComparator gyartoRendezo(){
+        return new GyartoComparator();
+    }
+    
+    
+    
     
 }
--- src/modell/Konzerv.java
+++ src/modell/Konzerv.java
@@ -1,5 +1,17 @@
 package modell;
 
+import java.io.File;
+
 public abstract class Konzerv extends Elelmiszer{
+    private String leiras;
+    private File recept;
+
+    public Konzerv(String nev, String gyarto) throws HibasDatumException {
+        super(nev, gyarto);
+    }
+
+    public void receptMutat(){
+        if(recept.exists()){}
+    }
     
 }
