e0fbddb734a986bd881aef15f2b5e194b5304b3c
bartuczpetra <bartucz.petra@gmail.com>
2022-05-17 14:32:26 +0200

    Konzerv osztaly javitasa

--- src/modell/Konzerv.java
+++ src/modell/Konzerv.java
@@ -8,8 +8,13 @@ public class Konzerv extends Elelmiszer{
     private File recept;
 
     public Konzerv(String leiras, String nev, String gyarto, LocalDate lejaratiDatum) throws HibasDatumException {
+        this(leiras,new File("nincs"),nev,gyarto,lejaratiDatum);
+    }
+    
+    public Konzerv(String leiras, File recept, String nev, String gyarto, LocalDate lejaratiDatum) throws HibasDatumException {
         super(nev, gyarto, lejaratiDatum);
         this.leiras = leiras;
+        this.recept = recept;
     }
     
     public void receptMutat(){
